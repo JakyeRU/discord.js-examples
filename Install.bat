a8c436c1684dac5d381819657fab0b7efc165ec0
JakyeRU <jakyeru@gmail.com>
2021-07-31 13:30:08 +0300

    Updated Install.bat The script will automatically delete after it has been used.

--- Install.bat
+++ Install.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 
 copy .env.example .env
-npm install && echo Installation successful. Please set your Discord token in .env
+npm install && echo Installation successful. Please set your Discord token in .env && (goto) 2>nul & del "%~f0"
